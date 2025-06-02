df348e5785864afaabede543fe6a77580bcdc0c4
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-06-02 11:52:09 +0300

    Add initial function

---
+++ lessons/init.go
@@ -0,0 +1,15 @@
+package lessons
+
+import (
+	"fmt"
+)
+
+// Init initializes the lessons package and can be used to set up any necessary state or configurations.
+func Init() {
+	fmt.Println("Initializing lessons package...")
+	// You can add any setup code here if needed
+}
+func jj() {
+	fmt.Println("Starting main function execution")
+
+}
